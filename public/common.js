19b00623cc1581d9166e8b52bcfb08714465b3f2
1poundSOCKS <mathew.coburn@gmail.com>
2022-02-17 15:24:33 +0000

    Added a private page for upload of new topo images

---
+++ public/common.js
@@ -0,0 +1,30 @@
+let LoadImage = (location, filename) => new Promise( (resolve, reject) => {
+  const url = location + filename;
+  const img = new Image();
+  img.onload = () => resolve(img);
+  img.onerror = (err) => reject(err);
+  img.src = url;
+});
+
+let LoadTopoImages = (crag, location) => new Promise( (resolve, reject) => {
+  let loaders = crag.topos.map(topo => LoadImage(location, topo.imageFile));
+  Promise.all(loaders)
+  .then(images => {
+    crag.topos.forEach( (topo, index) => {
+      topo.image = images[index];
+    });
+    resolve(crag);
+  })
+  .catch(err => reject(err));
+});
+
+let LoadCrag = (url, imagesLocation) => new Promise( (resolve, reject) => {
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    LoadTopoImages(data, imagesLocation).then(crag => {
+      resolve(crag);
+    });
+  })
+  .catch(err => reject(err));
+});
