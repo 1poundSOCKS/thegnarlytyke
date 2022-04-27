10cdaceb1caa0c23b5fd1d3bb64a1ce7651b58ea
1poundSOCKS <mathew.coburn@gmail.com>
2022-04-27 11:08:57 +0100

    Bug stopping new routes being marked fixed

--- server/source/objects/route.cjs
+++ server/source/objects/route.cjs
@@ -1,13 +1,13 @@
 let uuid = require('uuid');
 
 let Route = function(route) {
-  this.id = route.id;
-  this.points = route.points ? route.points : [];
+  this.route = route;
 }
 
 Route.prototype.AppendPoint = function(x, y) {
-  const pointCount = this.points.push({id: uuid.v4(), x: x, y: y});
-  return this.points[pointCount-1];
+  if( !this.route.points ) this.route.points = [];
+  const pointCount = this.route.points.push({id: uuid.v4(), x: x, y: y});
+  return this.route.points[pointCount-1];
 }
 
 module.exports = Route;
--- server/test/objects/route.test.js
+++ server/test/objects/route.test.js
@@ -1,20 +1,22 @@
 const Route = require('../../source/objects/route.cjs');
 
 test("AppendPoint: append the first point", () => {
-  const route = new Route({id: 'r1'});
+  const routeData = {id: 'r1'}
+  const route = new Route(routeData);
   const point = route.AppendPoint(1, 2);
   expect(point.id.length).toBeGreaterThan(0);
   expect(point.x).toEqual(1);
   expect(point.y).toEqual(2);
-  expect(route.points).toEqual([point]);
+  expect(routeData.points).toEqual([point]);
 });
 
 test("AppendPoint: append two points", () => {
-  const route = new Route({id: 'r1'});
+  const routeData = {id: 'r1'}
+  const route = new Route(routeData);
   const point1 = route.AppendPoint(1, 2);
   const point2 = route.AppendPoint(3, 4);
   expect(point2.id.length).toBeGreaterThan(0);
   expect(point2.x).toEqual(3);
   expect(point2.y).toEqual(4);
-  expect(route.points).toEqual([point1, point2]);
+  expect(routeData.points).toEqual([point1, point2]);
 });
