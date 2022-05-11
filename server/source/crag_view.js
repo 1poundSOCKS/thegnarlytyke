be63856f2112d039b5986481ae3c50c3ade79e9b
1poundSOCKS <mathew.coburn@gmail.com>
2022-05-11 13:44:37 +0100

    Refactor of topo view

--- server/source/crag_view.js
+++ server/source/crag_view.js
@@ -6,8 +6,6 @@ const CragLoader = require('./objects/crag-loader.cjs');
 const TopoMediaScroller = require('./objects/topo-media-scroller.cjs');
 
 let _crag = new Crag();
-let _topoImages = new Map();
-let _selectedTopoImageContainer = null;
 let _contentEditable = false;
 let _topoMediaScroller = null;
 let _mainTopoImage = null;
@@ -18,7 +16,7 @@ module.exports = SetViewContentEditable = editable => {
 }
 
 module.exports = LoadAndDisplayCrag = async (cragID, headerElement) => {
-  _topoMediaScroller = new TopoMediaScroller(document.getElementById('topo-images-container'), _crag, _contentEditable);
+  _topoMediaScroller = new TopoMediaScroller(document.getElementById('topo-images-container'), _crag, _contentEditable, OnTopoSelected);
   _mainTopoImage = new TopoImage(document.getElementById('main-topo-image'));
   _mainTopoImage.contentEditable = _contentEditable;
 
@@ -26,42 +24,12 @@ module.exports = LoadAndDisplayCrag = async (cragID, headerElement) => {
   const crag = await cragStorage.Load(cragID);
 
   if( headerElement && crag.name ) headerElement.innerText = crag.name;
-  
-  _crag.Attach(crag);
-  let cragTopoIDs = _crag.topos.map( topo => topo.id );
-  
-  let topoImageContainers = cragTopoIDs.map( topoID => {
-    return document.getElementById('topo-images-container').appendChild(CreateTopoImageContainer(topoID));
-  });
 
-  let topoImageCanvases = topoImageContainers.map( container => {
-    let topoCanvas = document.createElement('canvas')
-    topoCanvas.classList.add('topo-image');
-    topoCanvas = container.appendChild(topoCanvas);
-    topoCanvas.onclick = event => OnTopoSelected(event);
-    return topoCanvas;
-  });
-  
+  _crag.Attach(crag);
+
   const env = Config.environment;
   const imagesPath = `env/${env}/images/`;
-
-  const topoImageLoaders = [];
-  topoImageCanvases.forEach( async canvas => {
-    let topoID = canvas.parentElement.dataset.id;
-    let topo = _crag.GetMatchingTopo(topoID);
-    if( topo && topo.imageFile ) {
-      let topoImageLoader = LoadImage(`${imagesPath}${topo.imageFile}`);
-      topoImageLoaders.push(topoImageLoader);
-      let topoImage = await topoImageLoader;
-      _topoImages.set(topoID, topoImage);
-      DisplayTopoImage(canvas, topoImage);
-    }
-  });
-
-  if( topoImageContainers.length > 0 ) {
-    await topoImageLoaders[0];
-    SetCurrentTopoContainer(topoImageContainers[0]);
-  }
+  _topoMediaScroller.LoadTopoImages(imagesPath);
 
   RefreshCragRouteTable(_crag);
 }
@@ -71,57 +39,32 @@ module.exports = SaveCrag = async () => {
   cragStorage.Save(_crag);
 }
 
-module.exports = LoadImage = (url) => new Promise( (resolve, reject) => {
-  const img = new Image();
-  img.onload = () => resolve(img);
-  img.onerror = (err) => reject(err);
-  img.src = url;
-});
-
-let OnTopoSelected = event => {
-  SetCurrentTopoContainer(event.target.parentElement);
-}
-
-let SetCurrentTopoContainer = topoContainer => {
-  if( _selectedTopoImageContainer ) _selectedTopoImageContainer.classList.remove('topo-container-selected');
-
-  _selectedTopoImageContainer = topoContainer;
-  _selectedTopoImageContainer.classList.add('topo-container-selected');
-  let selectedTopoID = GetSelectedTopoID();
-
-  RefreshIcons();
-  RefreshMainTopoView();
-  RefreshTopoRouteTable(_crag, selectedTopoID);
-  RefreshCragRouteTable(_crag, selectedTopoID);
-
+let OnTopoSelected = (topoID, topoContainer) => {
+  RefreshIcons(topoContainer);
   document.getElementById('main-topo-container').classList.remove('do-not-display');
-
   if( _contentEditable ) _mainTopoImage.AddMouseHandler();
+  _mainTopoImage.image = _topoMediaScroller.topoImages.get(topoID);
+  _mainTopoImage.topo = _crag.GetMatchingTopo(topoID);
+  _mainTopoImage.Refresh();
+  RefreshTopoRouteTable(_crag, topoID);
+  if( _contentEditable ) {
+    RefreshCragRouteTable(_crag, topoID);
+    _mainTopoImage.AddMouseHandler();
+  }
 }
 
-module.exports = GetSelectedTopoID = () => _selectedTopoImageContainer?.dataset.id;
-module.exports = GetSelectedTopo = () => new Topo(_crag.GetMatchingTopo(GetSelectedTopoID()));
-
-
-module.exports = RefreshIcons = () => {
+module.exports = RefreshIcons = (topoContainer) => {
   const shiftTopoLeftContainer = document.getElementById('shift-topo-left-container');
   if( shiftTopoLeftContainer ) {
-    if( _selectedTopoImageContainer.previousSibling ) shiftTopoLeftContainer.classList.remove('do-not-display');
+    if( topoContainer.previousSibling ) shiftTopoLeftContainer.classList.remove('do-not-display');
     else shiftTopoLeftContainer.classList.add('do-not-display');
   }
 
   const shiftTopoRightContainer = document.getElementById('shift-topo-right-container');
   if( shiftTopoRightContainer ) {
-    if( _selectedTopoImageContainer.nextSibling ) shiftTopoRightContainer.classList.remove('do-not-display');
+    if( topoContainer.nextSibling ) shiftTopoRightContainer.classList.remove('do-not-display');
     else shiftTopoRightContainer.classList.add('do-not-display');
   }
-}
-
-module.exports = RefreshMainTopoView = () => {
-  let selectedTopoID = GetSelectedTopoID();
-  _mainTopoImage.image = _topoImages.get(selectedTopoID);
-  _mainTopoImage.topo = _crag.GetMatchingTopo(selectedTopoID);
-  _mainTopoImage.Refresh();
 }
 
 module.exports = AddTopo = () => {
@@ -154,26 +97,4 @@ module.exports = SortSelectedTopoRoutes = () => {
   topo.SortRoutesLeftToRight();
   RefreshTopoRouteTable(_crag, GetSelectedTopoID());
   _mainTopoImage.Refresh();
-}
-
-module.exports = CreateTopoImageContainer = (topoID) => {
-  let container = document.createElement('div');
-  container.classList.add('topo-container');
-  container.setAttribute('data-id', topoID);
-  return container;
-}
-
-module.exports = DisplayTopoImage = (topoCanvas, topoImage, heightInRem) => {
-  ResizeTopoCanvas(topoCanvas, topoImage, heightInRem);
-  let ctx = topoCanvas.getContext('2d');
-  ctx.drawImage(topoImage, 0, 0, topoCanvas.width, topoCanvas.height);
-  return topoCanvas;
-}
-
-module.exports = ResizeTopoCanvas = (topoCanvas, topoImage, heightInRem) => {
-  let height = heightInRem;
-  let width = topoImage.width * height / topoImage.height;
-  topoCanvas.setAttribute('width', topoImage.width);
-  topoCanvas.setAttribute('height', topoImage.height);
-  return topoCanvas;
 }
--- server/source/objects/topo-media-scroller.cjs
+++ server/source/objects/topo-media-scroller.cjs
@@ -1,8 +1,47 @@
-let TopoMediaScroller = function(element, crag, edit) {
+const Crag = require("./crag.cjs");
+
+let TopoMediaScroller = function(element, crag, edit, OnTopoSelectedCallback) {
   this.element = element;
   this.crag = crag;
   this.currentTopoContainer = null;
   this.edit = edit;
+  this.topoImages = new Map();
+  this.OnTopoSelectedCallback = OnTopoSelectedCallback;
+}
+
+TopoMediaScroller.prototype.LoadTopoImages = async function(imagesPath) {
+  let cragTopoIDs = this.crag.topos.map( topo => topo.id );
+  
+  let topoImageContainers = cragTopoIDs.map( topoID => {
+    return this.element.appendChild(this.CreateTopoImageContainer(topoID));
+  });
+
+  let topoImageCanvases = topoImageContainers.map( container => {
+    let topoCanvas = document.createElement('canvas')
+    topoCanvas.classList.add('topo-image');
+    topoCanvas = container.appendChild(topoCanvas);
+    topoCanvas.onclick = () => this.OnTopoSelected(container);
+    return topoCanvas;
+  });
+
+  const topoImageLoaders = [];
+  const crag = new Crag(this.crag);
+  topoImageCanvases.forEach( async canvas => {
+    let topoID = canvas.parentElement.dataset.id;
+    let topo = crag.GetMatchingTopo(topoID);
+    if( topo && topo.imageFile ) {
+      let topoImageLoader = this.LoadImage(`${imagesPath}${topo.imageFile}`);
+      topoImageLoaders.push(topoImageLoader);
+      let topoImage = await topoImageLoader;
+      this.topoImages.set(topoID, topoImage);
+      this.DisplayTopoImage(canvas, topoImage);
+    }
+  });
+
+  if( topoImageLoaders.length > 0 ) {
+    await topoImageLoaders[0];
+    this.OnTopoSelected(topoImageContainers[0]);
+  }
 }
 
 TopoMediaScroller.prototype.AddTopo = function(topo) {
@@ -14,22 +53,35 @@ TopoMediaScroller.prototype.AddTopo = function(topo) {
   return canvas;
 }
 
+TopoMediaScroller.prototype.CreateTopoImageContainer = function(topoID) {
+  let container = document.createElement('div');
+  container.classList.add('topo-container');
+  container.setAttribute('data-id', topoID);
+  return container;
+}
+
 TopoMediaScroller.prototype.OnTopoSelected = function(topoContainer) {
   if( this.currentTopoContainer ) this.currentTopoContainer.classList.remove('topo-container-selected');
-
   this.currentTopoContainer = topoContainer;
-
   this.currentTopoContainer.classList.add('topo-container-selected');
-  let selectedTopoID = GetSelectedTopoID();
+  this.OnTopoSelectedCallback(this.currentTopoContainer.dataset.id, this.currentTopoContainer);
+}
 
-  RefreshIcons();
-  RefreshMainTopoView();
-  RefreshTopoRouteTable(this.crag, selectedTopoID);
-  RefreshCragRouteTable(this.crag, selectedTopoID);
+TopoMediaScroller.prototype.DisplayTopoImage = function(topoCanvas, topoImage, heightInRem) {
+  topoCanvas.setAttribute('width', topoImage.width);
+  topoCanvas.setAttribute('height', topoImage.height);
+  let ctx = topoCanvas.getContext('2d');
+  ctx.drawImage(topoImage, 0, 0, topoCanvas.width, topoCanvas.height);
+  return topoCanvas;
+}
 
-  document.getElementById('main-topo-container').classList.remove('do-not-display');
-
-  if( this.edit ) _mainTopoImage.AddMouseHandler();
+TopoMediaScroller.prototype.LoadImage = function(url) {
+  return new Promise( (resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = (err) => reject(err);
+    img.src = url;
+  });
 }
 
 module.exports = TopoMediaScroller;
