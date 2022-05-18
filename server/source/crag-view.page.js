e7c9689cdac3cc33a4ebe95ad0f774ed411fedd6
1poundSOCKS <mathew.coburn@gmail.com>
2022-05-18 16:03:04 +0100

    Refactoring view crag page

--- server/source/crag-view.page.js
+++ server/source/crag-view.page.js
@@ -3,7 +3,8 @@ const CragLoader = require('./objects/crag-loader.cjs');
 const Crag = require('./objects/crag.cjs');
 const TopoMediaScroller = require('./objects/topo-media-scroller.cjs');
 const TopoImage = require('./objects/topo-image.cjs');
-require('./route_tables.js');
+const TopoRouteTable = require('./objects/topo-route-table.cjs');
+// require('./route_tables.js');
 
 let _crag = null;
 let _topoMediaScroller = null;
@@ -33,6 +34,8 @@ let OnConfigLoad = async () => {
   _topoMediaScroller = new TopoMediaScroller(document.getElementById('topo-images-container'), _crag, false, OnTopoSelected);
   _topoMediaScroller.LoadTopoImages(`env/${Config.environment}/images/`);
 
+  _topoRouteTable = new TopoRouteTable(document.getElementById('topo-route-table'));
+
   if( Config.mode === "edit" ) {
     document.getElementById('upload-topos-address').setAttribute('href', `./upload-topos.html?id=${cragID}`);
     document.getElementById('edit-topos-address').setAttribute('href', `./crag-edit.html?id=${cragID}`);
@@ -41,9 +44,12 @@ let OnConfigLoad = async () => {
 }
 
 let OnTopoSelected = (topoID, topoContainer) => {
+  const selectedTopo = new Crag(_crag).GetMatchingTopo(topoID);
   document.getElementById('main-topo-container').classList.remove('do-not-display');
   _mainTopoImage.image = _topoMediaScroller.topoImages.get(topoID);
-  _mainTopoImage.topo = new Crag(_crag).GetMatchingTopo(topoID);
+  _mainTopoImage.topo = selectedTopo;
   _mainTopoImage.Refresh();
-  RefreshTopoRouteTable(_crag, topoID);
+  _topoRouteTable.topo = selectedTopo;
+  _topoRouteTable.Refresh();
+  // RefreshTopoRouteTable(_crag, topoID);
 }
