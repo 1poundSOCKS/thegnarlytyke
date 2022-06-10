29517855fdad5aac0e13a25840f58d1b9ac936ca
1poundSOCKS <mathew.coburn@gmail.com>
2022-06-10 17:07:59 +0100

    API changes

--- api/github-actions-with-aws-sam/load_image/app.py
+++ api/github-actions-with-aws-sam/load_image/app.py
@@ -1,8 +1,7 @@
 import json
 import boto3
 
-
-def load_image(event, context):
+def lambda_handler(event, context):
 
     s3 = boto3.client('s3')
     print(event)
@@ -22,24 +21,4 @@ def load_image(event, context):
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
         "body": t
-    }
-
-def save_image(event, context):
-
-    string = event['body']
-    encoded_string = string.encode("utf-8")
-
-    s3 = boto3.client('s3')
-    request_data = event['queryStringParameters']
-    object_id = request_data['id']
-    data_key = "{}.txt".format(object_id)
-    s3.put_object(Bucket="images.thegnarlytyke.com", Key=data_key, Body=encoded_string)
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "save image call successful"
-            }
-        )
     }
