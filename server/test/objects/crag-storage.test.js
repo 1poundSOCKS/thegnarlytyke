1f858d983c9eaf8ebc6ea156fd2ef79612d188c6
1poundSOCKS <mathew.coburn@gmail.com>
2022-06-15 11:36:22 +0100

    Housekeeping

--- server/test/objects/crag-storage.test.js
+++ server/test/objects/crag-storage.test.js
@@ -1,4 +1,4 @@
-const CragLoader = require('../../source/objects/crag-loader.cjs');
+const CragStorage = require('../../source/objects/crag-storage.cjs');
 
 test("Parse a crag object to update attached point references", () => {
   const point1 = {id: 'p1', x: 1, y: 2};
@@ -6,7 +6,7 @@ test("Parse a crag object to update attached point references", () => {
   const route = {id: 'r1', points: [point1, point2]};
   const topo = {id: 't1', routes: [route]};
   const crag = {id: 'c1', topos: [topo]};
-  const cragLoader = new CragLoader();
+  const cragLoader = new CragStorage();
   cragLoader.UpdateCragAfterRestore(crag);
   expect(crag.topos[0].routes[0].points).toEqual([point1, {id: 'p2', attachedTo: point1}]);
 });
@@ -17,7 +17,7 @@ test("Format a crag object with attached points for storage", () => {
   const route = {id: 'r1', points: [point1, point2]};
   const topo = {id: 't1', routes: [route]};
   const crag = {id: 'c1', topos: [topo]};
-  const cragLoader = new CragLoader();
+  const cragLoader = new CragStorage();
   const cragForStorage = cragLoader.FormatCragForStorage(crag);
   expect(cragForStorage.topos[0].routes[0].points[1].attachedTo).toEqual('p1');
 });
@@ -28,14 +28,14 @@ test("Format a crag object for storage and ensure all properties included", () =
   const topoRoute = {id: 'tr1', points: [point]};
   const topo = {id: 't1', imageFile: 'image.topo.jpg', routes: [topoRoute]};
   const crag = {id: 'c1', name: 'Crag X', routes: [cragRoute], topos: [topo]};
-  const cragLoader = new CragLoader();
+  const cragLoader = new CragStorage();
   const cragForStorage = cragLoader.FormatCragForStorage(crag);
   expect(cragForStorage).toEqual(crag);
 });
 
 test("Format points for storage when there aren't any", () => {
   const topoRoutes = [{id: 'tr1'}];
-  const cragStorage = new CragLoader();
+  const cragStorage = new CragStorage();
   const routesForStorage = cragStorage.FormatTopoRoutesForStorage(topoRoutes);
   expect(routesForStorage).toEqual([{id: 'tr1', points:[]}]);
 });
@@ -43,14 +43,14 @@ test("Format points for storage when there aren't any", () => {
 test("Save a crag when the topo has no routes", () => {
   const topo1 = {id:'t1'};
   const crag = {topos:[topo1]};
-  const cragStorage = new CragLoader();
+  const cragStorage = new CragStorage();
   expect(cragStorage.FormatCragForStorage(crag)).toEqual(crag);
 });
 
 test("Restore a crag with an empty topo", () => {
   const topo1 = {id:'t1'};
   const crag = {topos:[topo1]};
-  const cragStorage = new CragLoader();
+  const cragStorage = new CragStorage();
   cragStorage.UpdateCragAfterRestore(crag);
   expect(crag).toEqual({topos:[topo1]});
 });
