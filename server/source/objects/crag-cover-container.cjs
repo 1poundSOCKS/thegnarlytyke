9885d9192ee3497f988f3e3a55d001fefcef7117
1poundSOCKS <mathew.coburn@gmail.com>
2022-06-23 09:54:14 +0100

    Crag covers updated

--- server/source/objects/crag-cover-container.cjs
+++ server/source/objects/crag-cover-container.cjs
@@ -6,11 +6,6 @@ let CragCoverContainer = function(crag, imageURL) {
   this.element.classList.add("crag-cover-container")
   this.element.setAttribute('data-id', crag.id);
 
-  this.header = document.createElement('h3');
-  this.header.classList.add("crag-cover-header");
-  this.header.innerText = crag.name;
-  this.element.appendChild(this.header)
-
   if( crag.imageLoader ) {
     this.canvas = document.createElement('canvas');
     this.canvas.classList.add("crag-cover-image");
@@ -20,7 +15,16 @@ let CragCoverContainer = function(crag, imageURL) {
       this.canvas.height = image.height;
       const ctx = this.canvas.getContext("2d");
       ctx.drawImage(image,0, 0);
-    })
+      const fontSize = 3;
+      ctx.font = `bold ${fontSize}rem six caps`;
+      const metrics = ctx.measureText(crag.name);
+      let widthOfText = metrics.width;
+      const xPosOfText = ctx.canvas.width /  2 - widthOfText / 2;
+      ctx.fillStyle = 'rgba(225,225,225,0.6)';
+      ctx.fillRect(0,0,ctx.canvas.width,60);
+      ctx.fillStyle = "rgb(30,30,30)";
+      ctx.fillText(crag.name, xPosOfText, 50);
+     })
   }
   else if( crag.imageFile ) {
     this.image = document.createElement('img');
