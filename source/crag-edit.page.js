32a0ea466bae85e09d5e068ed1fecf5fbdc4bae5
1poundSOCKS <mathew.coburn@gmail.com>
2022-08-17 18:59:31 +0100

    x

--- source/crag-edit.page.js
+++ source/crag-edit.page.js
@@ -33,6 +33,12 @@ let OnConfigLoad = async () => {
 
   new PageHeader(document.getElementById('page-header-container'),'edit',_cookie,Config)
 
+  const page = document.getElementById('page')
+  page.appendChild(CreateIconBarContainer())
+  page.appendChild(CreateCragIndexContainer())
+  page.appendChild(CreateCragViewContainer())
+  page.appendChild(CreateTopoEditContainer())
+
   DataStorage.Init(Config, _cookie.GetValue("user-id"), _cookie.GetValue("user-token"),true);
   ImageStorage.Init(Config, _cookie.GetValue("user-id"), _cookie.GetValue("user-token"));
 
@@ -110,4 +116,87 @@ let OnPublishUserUpdates = () => {
     console.log('published!')
   })
   .catch( err => console.log(err) )
+}
+
+let CreateIconBarContainer = () => {
+  const element = document.createElement('div')
+  element.id = 'icon-bar-container'
+  element.classList.add('icon-bar-container')
+  element.style = 'display:none'
+
+  element.appendChild(CreateIcon('icon-add','add')).classList.add('far','fa-plus-square')
+  element.appendChild(CreateIcon('icon-update-image','update image')).classList.add('fas','fa-file-upload')
+  element.appendChild(CreateIcon('icon-shift-left','shift left')).classList.add('fas','fa-arrow-left')
+  element.appendChild(CreateIcon('icon-shift-right','shift right')).classList.add('fas','fa-arrow-right')
+  element.appendChild(CreateIcon('icon-edit','edit')).classList.add('fas','fa-edit')
+  element.appendChild(CreateIcon('icon-save','save')).classList.add('fas','fa-save')
+  element.appendChild(CreateIcon('icon-publish','publish')).innerText = 'publish'
+  element.appendChild(CreateIcon('icon-close','close')).classList.add('far','fa-window-close')
+
+  return element
+}
+
+let CreateIcon = (id,title) => {
+  const icon = document.createElement('i')
+  icon.id = id
+  icon.title = title
+  return icon
+}
+
+let CreateCragIndexContainer = () => {
+  const element = document.createElement('div')
+  element.id = 'crag-index-container'
+
+    const statusBarContainer = document.createElement('div')
+    statusBarContainer.classList.add('crag-view-header')
+    statusBarContainer.style = 'display:none'
+
+      const status = document.createElement('input')
+      status.id = 'status'
+      status.classList.add('status-bar')
+      status.type = 'text'
+      status.name = 'name'
+      status.size = 20
+      status.readOnly = true
+  
+    statusBarContainer.appendChild(status)
+
+  element.appendChild(statusBarContainer)
+
+  const cragCoversContainer = document.createElement('div')
+  cragCoversContainer.id = 'crag-covers-container'
+  element.appendChild(cragCoversContainer)
+
+  return element
+}
+
+let CreateCragViewContainer = () => {
+  const element = document.createElement('div')
+  element.id = 'crag-view-container'
+  element.style = 'display:none'
+
+    const cragViewHeader = document.createElement('div')
+    cragViewHeader.id = 'crag-view-header'
+    cragViewHeader.classList.add('crag-view-header')
+
+      const cragName = document.createElement('input')
+      cragName.id = 'crag-name'
+      cragName.classList.add('crag-name')
+  
+    cragViewHeader.appendChild(cragName)
+
+  element.appendChild(cragViewHeader)
+
+  const topoImagesContainer = document.createElement('div')
+  topoImagesContainer.id = 'topo-images-container'
+  topoImagesContainer.classList.add('topo-images-container-edit')
+  element.appendChild(topoImagesContainer)
+
+  return element
+}
+
+let CreateTopoEditContainer = () => {
+  const element = document.createElement('div')
+  element.id = 'topo-edit-container'
+  return element
 }
