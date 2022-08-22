bc2e43c7964c2fd2e3dae4144eb3b5ceab650b51
1poundSOCKS <mathew.coburn@gmail.com>
2022-08-22 08:17:46 +0100

    Deploy to prod script includes the config

--- scripts/deploy-to-prod.bat
+++ scripts/deploy-to-prod.bat
@@ -1,2 +1,3 @@
 aws s3 sync s3://test.thegnarlytyke.com s3://thegnarlytyke.com --exclude "config.json" --delete
+aws s3 sync ../config/prod s3://thegnarlytyke.com
 aws cloudfront create-invalidation --distribution-id E36NDLK7X4PLKS --paths "/*"
