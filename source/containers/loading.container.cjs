c567aa4e245286ecf7995195c218554060ed4248
1poundSOCKS <mathew.coburn@gmail.com>
2022-08-28 12:14:52 +0100

    More edit page UI tweaks

--- source/containers/loading.container.cjs
+++ source/containers/loading.container.cjs
@@ -1,8 +1,21 @@
 let Create = () => {
-  const div = document.createElement('div')
-  div.classList.add('loading-container')
-  div.innerText = 'loading...'
-  return {root:div}
+  const root = document.createElement('div')
+  root.classList.add('loading-container')
+
+  const header = document.createElement('div')
+
+  // div.innerText = 'loading...'
+
+  // <i class="fa fa-spinner" aria-hidden="true"></i>
+
+  const spinner = document.createElement('i')
+  spinner.classList.add('fa','fa-spinner','fa-spin')
+
+  root.appendChild(header)
+  root.appendChild(spinner)
+
+  return {root:root,header:header}
 }
 
 exports.Create = Create
+exports.DisplayTemporaryView = DisplayTemporaryView
