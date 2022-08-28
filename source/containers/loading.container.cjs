f0d2008091de673c289f2b2c49c38906980573e7
1poundSOCKS <mathew.coburn@gmail.com>
2022-08-28 12:23:15 +0100

    Small fix to edit

--- source/containers/loading.container.cjs
+++ source/containers/loading.container.cjs
@@ -4,10 +4,6 @@ let Create = () => {
 
   const header = document.createElement('div')
 
-  // div.innerText = 'loading...'
-
-  // <i class="fa fa-spinner" aria-hidden="true"></i>
-
   const spinner = document.createElement('i')
   spinner.classList.add('fa','fa-spinner','fa-spin')
 
@@ -18,4 +14,3 @@ let Create = () => {
 }
 
 exports.Create = Create
-exports.DisplayTemporaryView = DisplayTemporaryView
